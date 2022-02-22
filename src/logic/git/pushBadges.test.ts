647059736acec89a587b524e668e36ef255356a3
Chris Odicho <chris.odicho@gmail.com>
2022-02-22 13:49:09 +1100

    feat: add custom commitMessage input

--- src/logic/git/pushBadges.test.ts
+++ src/logic/git/pushBadges.test.ts
@@ -1,20 +1,26 @@
+import { getInput } from '@actions/core';
 import { exec } from '@actions/exec';
+import { mocked } from 'jest-mock';
 
 import { pushBadges } from './pushBadges';
 
+jest.mock('@actions/core');
 jest.mock('@actions/exec');
 
 describe('pushBadges function', () => {
   beforeEach(() => jest.clearAllMocks());
 
   it('should push changes', async () => {
+    mocked(getInput).mockReturnValueOnce(
+      'chore(badges): update jest coverage badges [skip-ci]',
+    );
     await pushBadges();
 
     expect(exec).toHaveBeenCalledTimes(3);
     expect(exec).toHaveBeenNthCalledWith(1, 'git add', ['./badges']);
     expect(exec).toHaveBeenNthCalledWith(2, 'git commit', [
       '-m',
-      'Updating coverage badges',
+      'chore(badges): update jest coverage badges [skip-ci]',
     ]);
     expect(exec).toHaveBeenNthCalledWith(3, 'git push');
   });
--- src/logic/git/pushBadges.ts
+++ src/logic/git/pushBadges.ts
@@ -1,7 +1,8 @@
+import { getInput } from '@actions/core';
 import { exec } from '@actions/exec';
 
 export const pushBadges = async (): Promise<void> => {
   await exec('git add', ['./badges']);
-  await exec('git commit', ['-m', 'Updating coverage badges']);
+  await exec('git commit', ['-m', getInput('commitMessage')]);
   await exec('git push');
 };
