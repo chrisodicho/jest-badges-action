c76352d5595e9d0b172d1651f89378984ea6aea4
Chris Odicho <chris.odicho@gmail.com>
2022-11-07 19:13:42 +1100

    feat: extend support for github-token input

--- src/logic/git/pushBadges.ts
+++ src/logic/git/pushBadges.ts
@@ -1,8 +1,18 @@
 import { getInput } from '@actions/core';
 import { exec } from '@actions/exec';
+import { context } from '@actions/github';
 
 export const pushBadges = async (): Promise<void> => {
   await exec('git add', ['./badges']);
   await exec('git commit', ['-m', getInput('commit-message')]);
-  await exec('git push');
+
+  const githubToken = getInput('github-token');
+
+  if (githubToken) {
+    await exec(
+      `git push https://${githubToken}@github.com/${context.repo.owner}/${context.repo.repo}.git`,
+    );
+  } else {
+    await exec('git push');
+  }
 };
